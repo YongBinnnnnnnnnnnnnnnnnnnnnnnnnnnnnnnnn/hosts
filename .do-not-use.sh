ce5877ba619ed9603d771a22d32da76a7eb43e97
Bin Yong <binaryy@163.com>
2024-10-30 21:45:00 +0000

    Update .do-not-use.sh

--- .do-not-use.sh
+++ .do-not-use.sh
@@ -2,4 +2,9 @@
 
 echo "DO NOT USE THIS SCRIPT"
 echo "IT IS MADE FOR TESTING PURPOSE ONLY."
-#https://www.digwebinterface.com/?hostnames=github.com&type=&ns=resolver&useresolver=9.9.9.10&nameservers=
+exit 0
+
+echo "0.0.0.0 news.163.com" >> /etc/hosts
+echo "0.0.0.0 assets.msn.cn" >> /etc/hosts
+#
+curl "https://www.digwebinterface.com/?hostnames=github.com&type=a&ns=resolver&useresolver=9.9.9.10&nameservers="|grep 'none">github'|sed -r -e "s|.*\t([^<]*).*|\1 github.com|"|sudo tee /etc/hosts
