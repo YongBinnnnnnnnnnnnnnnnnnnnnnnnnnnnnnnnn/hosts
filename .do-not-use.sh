914ce96cd440cd55c5393865f25785e22e656161
Bin Yong <binaryy@163.com>
2024-10-30 21:36:04 +0000

    Create .do-not-use.sh

---
+++ .do-not-use.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "DO NOT USE THIS SCRIPT"
+echo "IT IS MADE FOR TESTING PURPOSE ONLY."
+#https://www.digwebinterface.com/?hostnames=github.com&type=&ns=resolver&useresolver=9.9.9.10&nameservers=
