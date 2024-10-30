65c1c61d59ddd5b96e66363118c98af2e507d4b3
Bin Yong <binaryy@163.com>
2024-10-30 21:51:25 +0000

    Update .do-not-use.sh

--- .do-not-use.sh
+++ .do-not-use.sh
@@ -7,4 +7,4 @@ exit 0
 echo "0.0.0.0 news.163.com" >> /etc/hosts
 echo "0.0.0.0 assets.msn.cn" >> /etc/hosts
 #
-curl "https://www.digwebinterface.com/?hostnames=github.com&type=a&ns=resolver&useresolver=9.9.9.10&nameservers="|grep 'none">github'|sed -r -e "s|.*\t([^<]*).*|\1 github.com|"|sudo tee /etc/hosts
+curl "https://www.digwebinterface.com/?hostnames=github.com&type=a&ns=resolver&useresolver=9.9.9.10&nameservers="|grep 'none">github'|sed -r -e "s|.*\t([^<]*).*|\1 github.com|"|sudo tee -a /etc/hosts
